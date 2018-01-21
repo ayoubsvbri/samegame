5ef5eb7ec4cd18fc53e8fc1883d0d17fd11b5dc4
Ayoub SABRI <34887622+AyoubSabri@users.noreply.github.com>
2018-01-21 22:44:02 +0100

    Delete jouerPartie.h

--- jouerPartie.h
+++
@@ -1,6 +0,0 @@
-void marquerBilleSupprimer(char y, int x);
-int verifierCasesAdjacentes(char y, int x);
-void marquerBlocSupprimer(char y, int x);
-int estCoupValide(char y, int x);
-void jouerCoup(void);
-int partieTerminee(void);
